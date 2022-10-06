bc02863942f739be9e295cf450cbe77a019014cc
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-10-06 19:49:26 +0200

    added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./styles/global.css";
 
 export default function App() {
   return (
     <>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
