ad650f4f13260db8fe6ae253aeb53dfff0abe6a0
Robin Schmidt <schmidt.rbn@gmail.com>
2022-10-09 20:03:07 +0200

    ADD: installed fonteawesome library, added icons to Header and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../styles/footer.css";
 
 export default function Footer() {
   return (
     <section className="footer">
-      <ul>
+      <ul className="exanple-container">
         <NavLink className="nav-link" to="/">
           <li>Beispiel</li>
         </NavLink>
@@ -17,6 +18,28 @@ export default function Footer() {
         <NavLink className="nav-link" to="/">
           <li>Beispiel</li>
         </NavLink>
+      </ul>
+      <ul className="social-container">
+        <li>
+          <a href="https://www.youtube.com/" target="_blank">
+            <FontAwesomeIcon className="fontawesomeicon" icon={['fab', 'youtube']} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/" target="_blank">
+            <FontAwesomeIcon className="fontawesomeicon" icon={['fab', 'facebook']} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/" target="_blank">
+            <FontAwesomeIcon className="fontawesomeicon" icon={['fab', 'instagram']} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/" target="_blank">
+            <FontAwesomeIcon className="fontawesomeicon" icon={['fab', 'twitter']} />
+          </a>
+        </li>
       </ul>
     </section>
   );
