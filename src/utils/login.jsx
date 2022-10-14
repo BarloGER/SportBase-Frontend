8400503bb1e72d7a19d1c9f23295c34921a2be21
Niklas <Niklas-Riebesell@outlook.de>
2022-10-14 09:56:29 +0200

    changed Posts and ErrorMessage for SignUp

--- src/utils/login.jsx
+++ src/utils/login.jsx
@@ -2,13 +2,16 @@ import axios from "axios";
 
 export const login = async (formSubmission) => {
   try {
-    const post = await axios.post(`http://localhost:8080/user`, {
-      email: formSubmission.email,
-      password: formSubmission.password,
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
+    const post = await axios.post(
+      `${process.env.REACT_APP_BLOG_API}/user/login`,
+      {
+        email: formSubmission.email,
+        password: formSubmission.password,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
     console.log(post);
   } catch (error) {
     console.log(error.response);
