396069c2a42d4d4366265703f9b9483c890e9c58
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-10-17 10:36:45 +0200

    improved searchbar functionality, added Account.js, fixed some classes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Events from "./components/Events";
 import EventForm from "./components/EventForm";
 import Dashboard from "./components/Dashboard";
 import Search from "./components/Search";
+import Account from "./components/Account";
 import "./styles/global.css";
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/eventform" element={<EventForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/account/:id" element={<Account />} />
       </Routes>
 
       <Footer />
