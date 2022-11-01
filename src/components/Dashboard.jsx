036e2409220effce14218f39dc48cd3cb45dbce9
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-11-01 12:46:02 +0100

    removed svg errors from sidebar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -44,13 +44,13 @@ function Dashboard({ user }) {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="w-6 h-6"
+              className="w-6 h-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
               />
             </svg>
