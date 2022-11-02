143bffa7164bc654c036a3ca66193e110db6a7d0
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-11-02 14:28:51 +0100

    added Label to Account.js and TeamProfile.js

--- src/components/TeamProfile.js
+++ src/components/TeamProfile.js
@@ -15,7 +15,9 @@ export default function TeamProfile({ user }) {
 
   const getTeamById = async () => {
     try {
-      const { data } = await axios.get(`${process.env.REACT_APP_FP_API}/team/${id}`)
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_FP_API}/team/${id}`
+      );
       setCurrentTeam(data);
       setIsLoading(false);
     } catch (error) {
@@ -25,7 +27,10 @@ export default function TeamProfile({ user }) {
   };
 
   const checkIfAllowed = () => {
-    if (!!user && (currentTeam.trainer === `${user.firstname} ${user.lastname}`)) {
+    if (
+      !!user &&
+      currentTeam.trainer === `${user.firstname} ${user.lastname}`
+    ) {
       setIsAllowed(true);
     } else {
       setIsAllowed(false);
@@ -53,10 +58,10 @@ export default function TeamProfile({ user }) {
   const handleJoinTeam = (e) => {
     e.preventDefault();
 
-    if (currentTeam.member.some(m => m._id === user._id)) {
-      console.log('you are already part of this team');
-    } else if (user.team && user.team !== '') {
-      console.log('you are already part of an other team');
+    if (currentTeam.member.some((m) => m._id === user._id)) {
+      console.log("you are already part of this team");
+    } else if (user.team && user.team !== "") {
+      console.log("you are already part of an other team");
     } else {
       const updatedTeam = { ...currentTeam };
       updatedTeam.member.push(user);
@@ -68,7 +73,7 @@ export default function TeamProfile({ user }) {
       updateCurrentTeam(updatedTeam);
       updateCurrentUser(updatedUser);
     }
-  }
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -106,9 +111,9 @@ export default function TeamProfile({ user }) {
                 }
                 alt={currentTeam.logoUrl ? currentTeam.team : "Vereinslogo"}
               />
-              <button className="btn"
-                onClick={handleJoinTeam}
-              >Verein Beitreten</button>
+              <button className="btn" onClick={handleJoinTeam}>
+                Verein Beitreten
+              </button>
             </div>
             <div className="user-aboutMe">
               <h2>Über uns</h2>
@@ -123,39 +128,51 @@ export default function TeamProfile({ user }) {
             </div>
           </div>
           <div className="right-container">
-            <div className="user-info">
-              <input
-                type="text"
-                name="teamname"
-                defaultValue={currentTeam.team}
-                readOnly={!isAllowed ? "readOnly" : ""}
-                required
-                onChange={(e) => handleInputChange(e)}
-              ></input>
-              <input
-                type="text"
-                name="sport"
-                defaultValue={currentTeam.sport}
-                readOnly={!isAllowed ? "readOnly" : ""}
-                required
-                onChange={(e) => handleInputChange(e)}
-              ></input>
-              <input
-                type="text"
-                name="trainer"
-                defaultValue={currentTeam.trainer}
-                placeholder="Trainer"
-                readOnly={!isAllowed ? "readOnly" : ""}
-              ></input>
-              <input
-                className={isAllowed ? "" : "btn-hidden"}
-                type="text"
-                name="logoUrl"
-                defaultValue={currentTeam.logoUrl}
-                placeholder="Vereinslogo"
-                readOnly={!isAllowed ? "readOnly" : ""}
-                onChange={(e) => handleInputChange(e)}
-              ></input>
+            <div className="team-info">
+              <label>
+                Vereinsname
+                <input
+                  type="text"
+                  name="teamname"
+                  defaultValue={currentTeam.team}
+                  readOnly={!isAllowed ? "readOnly" : ""}
+                  required
+                  onChange={(e) => handleInputChange(e)}
+                ></input>
+              </label>
+              <label>
+                Sportart
+                <input
+                  type="text"
+                  name="sport"
+                  defaultValue={currentTeam.sport}
+                  readOnly={!isAllowed ? "readOnly" : ""}
+                  required
+                  onChange={(e) => handleInputChange(e)}
+                ></input>
+              </label>
+              <label>
+                Trainer
+                <input
+                  type="text"
+                  name="trainer"
+                  defaultValue={currentTeam.trainer}
+                  placeholder="Trainer"
+                  readOnly={!isAllowed ? "readOnly" : ""}
+                ></input>
+              </label>
+              <label className={isAllowed ? "" : "btn-hidden"}>
+                Vereinslogo
+                <input
+                  className={isAllowed ? "" : "btn-hidden"}
+                  type="text"
+                  name="logoUrl"
+                  defaultValue={currentTeam.logoUrl}
+                  placeholder="Vereinslogo"
+                  readOnly={!isAllowed ? "readOnly" : ""}
+                  onChange={(e) => handleInputChange(e)}
+                ></input>
+              </label>
               <button
                 className={isAllowed ? "btn" : "btn-hidden"}
                 disabled={!isAllowed}
@@ -165,7 +182,9 @@ export default function TeamProfile({ user }) {
             </div>
             <div className="event-container">
               <h2>Mitglieder</h2>
-              <span><p>aktuell: {currentTeam.memberCount}</p></span>
+              <span>
+                <p>aktuell: {currentTeam.memberCount}</p>
+              </span>
               {currentTeam.member ? (
                 currentTeam.member.map((m) => (
                   <Link to={`/account/${m._id}`} key={m._id}>
